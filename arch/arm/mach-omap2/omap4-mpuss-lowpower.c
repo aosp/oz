f0910331ffed6533e5013a64454c911269f7e53a
Axel Haslam <axelhaslam@ti.com>
2011-02-21 13:36:58 -0600

    HACK: OMAP4: dont mark PPI and SPI interrupts as non-secure
    This is done to prevent a security fault while booing
    on EMU devices. For EMU, Dont mark interrupts as non-secure,
    until OFF is supported for EMU devices.
    
    Signed-off-by: Axel Haslam <axelhaslam@ti.com>

--- arch/arm/mach-omap2/omap4-mpuss-lowpower.c
+++ arch/arm/mach-omap2/omap4-mpuss-lowpower.c
@@ -421,11 +421,12 @@ int __init omap4_mpuss_init(void)
 	 * program the SAR locations for interrupt security registers to
 	 * reflect the same.
 	 */
-	sar_writel(GIC_ISR_NON_SECURE, ICDISR_CPU0_OFFSET, 0);
-	sar_writel(GIC_ISR_NON_SECURE, ICDISR_CPU1_OFFSET, 0);
-	for (i = 0; i < max_spi_reg; i++)
-		sar_writel(GIC_ISR_NON_SECURE, ICDISR_SPI_OFFSET, i);
-
+	if (omap_type() == OMAP2_DEVICE_TYPE_GP) {
+		sar_writel(GIC_ISR_NON_SECURE, ICDISR_CPU0_OFFSET, 0);
+		sar_writel(GIC_ISR_NON_SECURE, ICDISR_CPU1_OFFSET, 0);
+		for (i = 0; i < max_spi_reg; i++)
+			sar_writel(GIC_ISR_NON_SECURE, ICDISR_SPI_OFFSET, i);
+	}
 #ifdef CONFIG_CACHE_L2X0
 	/*
 	 * Save the L2X0 AUXCTRL value to SAR memory. Its used to
