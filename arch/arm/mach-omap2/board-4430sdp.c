6192b98cec0317151612c12d157fea6ee7b6c5b1
Vikram Pandita <vikram.pandita@ti.com>
2011-03-16 20:06:37 -0700

    omap4: usb: musb: enable adb
    Enable adb operation on android kernel
    
    Change-Id: Iba537f6089a50350c767ea960810cefb2ecaed0c
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -541,7 +541,13 @@ static const struct ehci_hcd_omap_platform_data ehci_pdata __initconst = {
 
 static struct omap_musb_board_data musb_board_data = {
 	.interface_type		= MUSB_INTERFACE_UTMI,
-	.mode			= MUSB_OTG,
+#ifdef CONFIG_USB_GADGET_MUSB_HDRC
+	.mode		= MUSB_PERIPHERAL,
+#elif defined(CONFIG_USB_MUSB_HDRC_HCD)
+	.mode           = MUSB_HOST,
+#elif definded(CONFIG_USB_MUSB_OTG)
+	.mode		= MUSB_OTG,
+#endif
 	.power			= 100,
 };
 
