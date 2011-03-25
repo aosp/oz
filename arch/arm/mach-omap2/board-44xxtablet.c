9e5a28db0cfef492e1237673b75c0ba6d8fe55c0
Vikram Pandita <vikram.pandita@ti.com>
2011-03-25 00:45:50 -0700

    omap4: blaze tablet: fix hardware name
    AFS expects a short username that gets used
    to load the board specific init.xxxx.rc
    
    Change-Id: Ic02da3904953b70373c4a1465f1981b917b84c69
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- arch/arm/mach-omap2/board-44xxtablet.c
+++ arch/arm/mach-omap2/board-44xxtablet.c
@@ -1627,7 +1627,7 @@ static void __init omap_4430sdp_map_io(void)
 	omap44xx_map_common_io();
 }
 
-MACHINE_START(OMAP_BLAZE, "OMAP4430 Tablet board")
+MACHINE_START(OMAP_BLAZE, "OMAP4430")
 	.phys_io	= 0x48000000,
 	.io_pg_offst	= ((0xfa000000) >> 18) & 0xfffc,
 	.boot_params	= 0x80000100,
