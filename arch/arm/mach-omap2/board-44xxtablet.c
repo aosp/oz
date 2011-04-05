e2b287b48798717f8dee56755e1d5e4b18b56642
Dan Murphy <dmurphy@ti.com>
2011-04-05 11:16:22 -0500

    Blaze Tablet: Add qtouch board file entries
    Remove the atmel mxt224 board file entries
    Add qtouch board file entries
    
    Change-Id: I68dc8610ec6bed9655aa59a178fba33b8d07ab56
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- arch/arm/mach-omap2/board-44xxtablet.c
+++ arch/arm/mach-omap2/board-44xxtablet.c
@@ -27,7 +27,7 @@
 #include <linux/leds-omap4430sdp-display.h>
 #include <linux/delay.h>
 #include <linux/input/sfh7741.h>
-#include <linux/i2c/atmel_mxt224.h>
+#include <linux/qtouch_obp_ts.h>
 #include <linux/i2c/cma3000.h>
 
 #include <mach/hardware.h>
@@ -58,7 +58,7 @@
 #define LED_SEC_DISP_GPIO 27
 #define DSI2_GPIO_59	59
 #define DP_4430_GPIO_59         59
-
+#define OMAP4_TOUCH_IRQ_1		35
 
 #define LED_PWM2ON		0x03
 #define LED_PWM2OFF		0x04
@@ -386,15 +386,130 @@ static struct omap_dss_board_info blazetablet_dss_data = {
 };
 
 /* Atmel MXT224 TouchScreen Begin */
-static struct atmel_mxt224_platform_data atmel_mxt224_ts_platform_data[] = {
+static struct qtm_touch_keyarray_cfg blaze_tablet_key_array_data[] = {
 	{
-		.x_line = 17,
-		.y_line = 13,
-		.x_size = 1023,
-		.y_size = 767,
-		.blen = 1,
-		.threshold = 30,
-		.orient = 4,
+		.ctrl = 0,
+		.x_origin = 0,
+		.y_origin = 0,
+		.x_size = 0,
+		.y_size = 0,
+		.aks_cfg = 0,
+		.burst_len = 0,
+		.tch_det_thr = 0,
+		.tch_det_int = 0,
+		.rsvd1 = 0,
+	},
+};
+
+static void blaze_tablet_touch_init(void)
+{
+	gpio_request(OMAP4_TOUCH_IRQ_1, "atmel touch irq");
+	gpio_direction_input(OMAP4_TOUCH_IRQ_1);
+	omap_mux_init_signal("gpmc_ad11.gpio_35",
+			OMAP_PULL_ENA | OMAP_PULL_UP |
+			OMAP_WAKEUP_EN | OMAP_MUX_MODE3 |
+			OMAP_INPUT_EN | OMAP_PIN_OFF_INPUT_PULLUP);
+}
+
+static struct qtouch_ts_platform_data atmel_mxt224_ts_platform_data = {
+	.irqflags	= (IRQF_TRIGGER_FALLING | IRQF_TRIGGER_LOW),
+	.flags		= (QTOUCH_USE_MULTITOUCH | QTOUCH_FLIP_Y),
+	.abs_min_x	= 0,
+	.abs_max_x	= 768,
+	.abs_min_y	= 0,
+	.abs_max_y	= 1024,
+	.abs_min_p	= 0,
+	.abs_max_p	= 255,
+	.abs_min_w	= 0,
+	.abs_max_w	= 15,
+	.x_delta	= 1024,
+	.y_delta	= 768,
+	.nv_checksum	= 0xfaf5,
+	.fuzz_x		= 0,
+	.fuzz_y		= 0,
+	.fuzz_p		= 2,
+	.fuzz_w		= 2,
+	.hw_reset	= NULL,
+	.power_cfg	= {
+		.idle_acq_int	= 0x58,
+		.active_acq_int	= 0x58,
+		.active_idle_to	= 0x32,
+	},
+	.acquire_cfg	= {
+		.charge_time	= 0x0a,
+		.atouch_drift	= 0x05,
+		.touch_drift	= 0x14,
+		.drift_susp	= 0x14,
+		.touch_autocal	= 0x0a,
+		.sync		= 0,
+		.cal_suspend_time = 0x09,
+		.cal_suspend_thresh = 0x23,
+	},
+	.multi_touch_cfg	= {
+		.ctrl		= 0x83,
+		.x_origin	= 0,
+		.y_origin	= 0,
+		.x_size		= 0x11,
+		.y_size		= 0x0d,
+		.aks_cfg	= 0,
+		.burst_len	= 0x01,
+		.tch_det_thr	= 0x20,
+		.tch_det_int	= 0x2,
+		.mov_hyst_init	= 0x0,
+		.mov_hyst_next	= 0x0,
+		.mov_filter	= 0x9,
+		.num_touch	= 1,
+		.orient		= 0x00,
+		.mrg_timeout	= 0x01,
+		.merge_hyst	= 0x0a,
+		.merge_thresh	= 0x0a,
+		.amp_hyst = 0x0a,
+		 .x_res = 0x02ff,
+		 .y_res = 0x03ff,
+		 .x_low_clip = 0x00,
+		 .x_high_clip = 0x00,
+		 .y_low_clip = 0x00,
+		 .y_high_clip = 0x00,
+	},
+	.key_array      = {
+		.cfg		= blaze_tablet_key_array_data,
+		.num_keys   = ARRAY_SIZE(blaze_tablet_key_array_data),
+	},
+	.grip_suppression_cfg = {
+		.ctrl		= 0x00,
+		.xlogrip	= 0x00,
+		.xhigrip	= 0x00,
+		.ylogrip	= 0x00,
+		.yhigrip	= 0x00,
+		.maxtchs	= 0x00,
+		.reserve0   = 0x00,
+		.szthr1	= 0x00,
+		.szthr2	= 0x00,
+		.shpthr1	= 0x00,
+		.shpthr2	= 0x00,
+	},
+	.noise0_suppression_cfg = {
+		.ctrl		= 0x07,
+		.reserved	= 0x0000,
+		.gcaf_upper_limit = 0x000a,
+		.gcaf_lower_limit = 0xfff6,
+		.gcaf_valid	= 0x04,
+		.noise_thresh	= 0x08,
+		.reserved1	= 0x00,
+		.freq_hop_scale = 0x01,
+		.burst_freq_0	= 0x0a,
+		.burst_freq_1 = 0x0f,
+		.burst_freq_2 = 0x14,
+		.burst_freq_3 = 0x19,
+		.burst_freq_4 = 0x1e,
+		.num_of_gcaf_samples = 0x04,
+	},
+	.spt_cte_cfg = {
+		.ctrl = 0x00,
+		.command = 0x00,
+		.mode = 0x01,
+		.gcaf_idle_mode = 0x04,
+		.gcaf_actv_mode = 0x08,
 	},
 };
 /* End Atmel Touch screen */
@@ -743,9 +858,9 @@ static struct i2c_board_info __initdata tablet_i2c_3_boardinfo[] = {
 
 static struct i2c_board_info __initdata tablet_i2c_4_boardinfo[] = {
 	{
-		I2C_BOARD_INFO("atmel_mxt224", 0x4b),
-		.platform_data = &atmel_mxt224_ts_platform_data[0],
-		.irq = 35,
+		I2C_BOARD_INFO(QTOUCH_TS_NAME, 0x4b),
+		.platform_data = &atmel_mxt224_ts_platform_data,
+		.irq = OMAP_GPIO_IRQ(OMAP4_TOUCH_IRQ_1),
 	},
 	{
 		I2C_BOARD_INFO("bmp085", 0x77),
@@ -802,6 +917,7 @@ void omap_44xxtablet_init(void)
 	tablet_i2c_init();
 	omap4_display_init();
 	omap_disp_led_init();
+	blaze_tablet_touch_init();
 
 	platform_add_devices(blazetablet_devices,
 		ARRAY_SIZE(blazetablet_devices));
