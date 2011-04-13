28b80e31335422f9f8a7da53ebce2da069bc85a5
Dan Murphy <dmurphy@ti.com>
2011-04-13 09:11:26 -0500

    [ARM] Blaze Tablet: Sensor board file update for gyro
    Add the entries for the MPU3050 into the blaze tablet
    sensor board file
    
    Change-Id: I8647bd9305082b015363e3980d41e67866fc6f59
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- arch/arm/mach-omap2/board-44xx-tablet-sensors.c
+++ arch/arm/mach-omap2/board-44xx-tablet-sensors.c
@@ -24,6 +24,7 @@
 
 #include <linux/input/sfh7741.h>
 #include <linux/i2c/bma180.h>
+#include <linux/i2c/mpu3050.h>
 
 #include <plat/i2c.h>
 
@@ -31,6 +32,7 @@
 #include "mux.h"
 
 #define OMAP4_BMA180ACCEL_GPIO		178
+#define OMAP4_MPU3050GYRO_GPIO		2
 
 static struct bma180accel_platform_data bma180accel_platform_data = {
 	.ctrl_reg0	= 0x11,
@@ -43,6 +45,24 @@ static struct bma180accel_platform_data bma180accel_platform_data = {
 	.fuzz_x		= 25,
 	.fuzz_y		= 25,
 	.fuzz_z		= 25,
+};
+
+static void blaze_tablet_mpu3050_init(void)
+{
+	if (gpio_request(OMAP4_MPU3050GYRO_GPIO, "mpu3050") < 0) {
+		pr_err("%s: MPU3050 GPIO request failed\n", __func__);
+		return;
+	}
+	gpio_direction_input(OMAP4_MPU3050GYRO_GPIO);
+}
+
+static struct mpu3050gyro_platform_data mpu3050_platform_data = {
+	.irq_flags = (IRQF_TRIGGER_HIGH | IRQF_ONESHOT),
+	.slave_i2c_addr = 0x40,
+	.sample_rate_div = 0x00,
+	.dlpf_fs_sync = 0x10,
+	.interrupt_cfg = (MPU3050_INT_CFG_OPEN | MPU3050_INT_CFG_LATCH_INT_EN |
+		MPU3050_INT_CFG_MPU_RDY_EN | MPU3050_INT_CFG_RAW_RDY_EN),
 };
 
 static struct i2c_board_info __initdata blaze_tablet_i2c_bus3_sensor_info[] = {
@@ -62,10 +82,17 @@ static struct i2c_board_info __initdata blaze_tablet_i2c_bus4_sensor_info[] = {
 		I2C_BOARD_INFO("bma180_accel", 0x40),
 		.platform_data = &bma180accel_platform_data,
 	},
+	{
+		I2C_BOARD_INFO("mpu3050_gyro", 0x68),
+		.platform_data = &mpu3050_platform_data,
+		.irq = OMAP_GPIO_IRQ(OMAP4_MPU3050GYRO_GPIO),
+	},
 };
 
 int __init omap4_tablet_sensor_init(void)
 {
+	blaze_tablet_mpu3050_init();
+
 	i2c_register_board_info(3, blaze_tablet_i2c_bus3_sensor_info,
 		ARRAY_SIZE(blaze_tablet_i2c_bus3_sensor_info));
 	i2c_register_board_info(4, blaze_tablet_i2c_bus4_sensor_info,
