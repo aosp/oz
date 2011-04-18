7924e1dac4bbbe621a98a96528f23603b5d6b18a
Misael Lopez Cruz <misael.lopez@ti.com>
2011-04-18 18:45:17 -0500

    ASoC: omap4panda: Add initial support
    Add initial support for OMAP4 Panda. Panda board was sharing the
    same machine driver with SDP4430 board, but due to the several
    differences between these two boards a new machine driver specific
    for Panda is required.
    
    Change-Id: Ibe8ee727afde20b0b817fc54282d9a19406229d0
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -954,9 +954,9 @@ static int __init sdp4430_soc_init(void)
 {
 	int ret = 0;
 
-	if (!machine_is_omap_4430sdp() && !machine_is_omap4_panda() &&
+	if (!machine_is_omap_4430sdp() &&
 		!machine_is_omap_tabletblaze()) {
-		pr_debug("Not SDP4430, BlazeTablet or PandaBoard!\n");
+		pr_debug("Not SDP4430 or BlazeTablet\n");
 		return -ENODEV;
 	}
 	printk(KERN_INFO "SDP4430 SoC init\n");
