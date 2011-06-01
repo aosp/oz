b9af4c608e7820be4bee6192596facf4cfa70b40
Praneeth Bajjuri <praneeth@ti.com>
2011-06-01 17:58:12 -0500

    OMAP4: SMC 1.04 P5 Release
    This patch fixes SMC normal world driver to be more
    compliant with linux kernel coding style.
    
    SMC driver now registers synchronous crypto algorithms
    (AES-CBC and AES-CTR)
    
    This patch release also fixes issue #6188. postlinker failure
    if the secure world configuration file was missing yet an
    invalid output file was generated.
    
    Secure world configuration is mandatory now.
    
    Signed-off-by: Trusted Logic <smc_support@trusted-logic.com>
    
    Android-porting
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>
    
    Change-Id: Id7848817b4d3af03e2b65e0eae1f23f2e5170c81

--- security/smc/omap4/s_version.h
+++ security/smc/omap4/s_version.h
@@ -45,7 +45,7 @@
 * defines to set the version number. Else set these values to 0.
 */
 #define S_VERSION_ENG 0
-#define S_VERSION_PATCH 2
+#define S_VERSION_PATCH 5
 
 #ifdef S_VERSION_BUILD
 /* TRICK: detect if S_VERSION is defined but empty */
@@ -84,8 +84,8 @@
 	S_VERSION_OS \
 	S_VERSION_PLATFORM \
 	S_VERSION_MAIN \
-	_S_VERSION_ENG \
 	_S_VERSION_PATCH \
+	_S_VERSION_ENG \
 	"."  __STRINGIFY2(S_VERSION_BUILD) " " \
 	S_VERSION_VARIANT
 
