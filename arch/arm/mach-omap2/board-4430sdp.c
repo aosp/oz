3dcfdf24a19cbfb087f45b39671dbe0087449646
Oleksandr <oleksandr.kozaruk@ti.com>
2011-06-14 21:43:01 -0500

    [ARM] board-4430sdp.c FM Tx and modem audio.
        FM Tx and modem audio doesn't work after enabling USB host support.
        USB initialization function reconfigures pins used for FM Tx and
        modem audio. This patch set this pins unused for usb.
    
    Change-Id: I0a502870ba4335feb26c98f09fa68bf78117ca1a
    Signed-off-by: Oleksandr <oleksandr.kozaruk@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -1443,7 +1443,7 @@ static struct i2c_board_info __initdata sdp4430_i2c_4_boardinfo[] = {
 
 static struct usbhs_omap_platform_data usbhs_pdata __initconst = {
 	.port_mode[0] = OMAP_EHCI_PORT_MODE_PHY,
-	.port_mode[1] = OMAP_OHCI_PORT_MODE_PHY_6PIN_DATSE0,
+	.port_mode[1] = OMAP_USBHS_PORT_MODE_UNUSED,
 	.port_mode[2] = OMAP_USBHS_PORT_MODE_UNUSED,
 	.phy_reset  = false,
 	.reset_gpio_port[0]  = -EINVAL,
