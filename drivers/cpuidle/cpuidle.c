328d674dca2206b7499cb5bd1dda9412b8842e1a
Volodymyr Riazantsev <x0153367@ti.com>
2011-06-20 13:11:12 -0500

    OMAP4:PM:CPUIDLE enable all c-states by default
    Enabling all CPU c-states by default in idle path.
    
    echo 1 > /sys/devices/system/cpu/cpu0/cpuidle/enable_state --> enables
    echo 0 > /sys/devices/system/cpu/cpu0/cpuidle/enable_state --> disables
    
    Change-Id: I0b64a1dce229063e86736cf60489850b1946c914
    Signed-off-by: Volodymyr Riazantsev <x0153367@ti.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -180,8 +180,8 @@ int cpuidle_enable_device(struct cpuidle_device *dev)
 	}
 	dev->last_residency = 0;
 	dev->last_state = NULL;
-	/* by default keep it disabled */
-	dev->enable_state = 0;
+	/* enable state by default */
+	dev->enable_state = 1;
 
 	smp_wmb();
 
