b9736588b52134c3d107327306a8a932a460271f
Oleksandr Lugovyk <x0158320@ti.com>
2011-07-07 13:59:03 +0300

    OMAPLFB: fix: apply function called 2 times
    after improper merge apply function was called two times
    in a row.
    
    Change-Id: I7427f2abe7ceef158e441716c5c319f77351d9cd
    Signed-off-by: Oleksandr Lugovyk <x0158320@ti.com>

--- drivers/gpu/pvr/omaplfb/omaplfb_linux.c
+++ drivers/gpu/pvr/omaplfb/omaplfb_linux.c
@@ -205,7 +205,6 @@ static void OMAPLFBFlipDSSComp(OMAPLFB_SWAPCHAIN *psSwapChain,
 	struct fb_info * framebuffer = psDevInfo->psLINFBInfo;
 	struct omapfb_info *ofbi = FB2OFB(framebuffer);
 	unsigned long fb_offset;
-	int i;
 	int r;
 	struct omap_overlay *overlay;
 	struct dss2_ovl_info dss2_ovl;
@@ -214,10 +213,9 @@ static void OMAPLFBFlipDSSComp(OMAPLFB_SWAPCHAIN *psSwapChain,
 
 	/* Only one overlay for the moment*/
 	if (ofbi->num_overlays > 0) {
-		i = 0;
 		/* Always get the first overlay, we are not supposed
 			to add more with sysfs */
-		overlay = ofbi->overlays[i];
+		overlay = ofbi->overlays[0];
 
 		if (comp)
 			r = dsscomp_get_ovl(comp, overlay->id, &dss2_ovl);
@@ -226,7 +224,6 @@ static void OMAPLFBFlipDSSComp(OMAPLFB_SWAPCHAIN *psSwapChain,
 
 		if (r) {
 			struct omap_overlay_info overlay_info;
-			struct omap_overlay_manager *manager;
 			WARNING_PRINTK("Ovl%d not found, updating manually",
 				overlay->id);
 			overlay->get_overlay_info(overlay, &overlay_info);
@@ -235,25 +232,15 @@ static void OMAPLFBFlipDSSComp(OMAPLFB_SWAPCHAIN *psSwapChain,
 			overlay_info.vaddr = framebuffer->screen_base +
 				fb_offset;
 			overlay->set_overlay_info(overlay, &overlay_info);
-			manager = overlay->manager;
-			if (manager) {
-				/* No display attached to ovl, don't update */
-				if (manager->device)
-					manager->apply(manager);
-			}
 		} else {
 			dss2_ovl.ba = framebuffer->fix.smem_start + fb_offset;
-			dss2_ovl.uv = framebuffer->screen_base + fb_offset;
 			dsscomp_set_ovl(comp, &dss2_ovl);
-			r = dsscomp_apply(comp);
-			if (r)
-				WARNING_PRINTK("Ovl%d apply failed, novls %d",
-					overlay->id, ofbi->num_overlays);
 		}
 
 		r = dsscomp_apply(comp);
 		if (r)
-			WARNING_PRINTK("Overlay %d apply failed %lx", overlay->id, framebuffer->fix.smem_start + fb_offset);
+			WARNING_PRINTK("Overlay %d apply failed %lx", overlay->id,
+				framebuffer->fix.smem_start + fb_offset);
 	}
 }
 
