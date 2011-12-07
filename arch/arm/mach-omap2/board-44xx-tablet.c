a181ee291c92782108112bcd63b09bccca1a31f1
Ambresh K <ambresh@ti.com>
2011-12-07 16:27:05 +0530

    OMAP4: Tablet: Regulator: Gate VCXIO & VDAC resources during suspend.
    Set {VCXIO, VDAC}_CFG_TRANS sleep state to OFF.
    
    Change-Id: Icf543639ca9a6ec1ea58942f3532157f30a8703e
    Signed-off-by: Ambresh K <ambresh@ti.com>

--- arch/arm/mach-omap2/board-44xx-tablet.c
+++ arch/arm/mach-omap2/board-44xx-tablet.c
@@ -445,6 +445,10 @@ static struct regulator_init_data tablet_vcxio = {
 		.valid_ops_mask	 = REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 		.always_on	= true,
+		.state_mem = {
+			.disabled       = true,
+		},
+		.initial_state          = PM_SUSPEND_MEM,
 	},
 	.num_consumer_supplies	= ARRAY_SIZE(tablet_vcxio_supply),
 	.consumer_supplies	= tablet_vcxio_supply,
@@ -465,6 +469,10 @@ static struct regulator_init_data tablet_vdac = {
 		.valid_ops_mask	 = REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 		.always_on	= true,
+		.state_mem = {
+			.disabled       = true,
+		},
+		.initial_state          = PM_SUSPEND_MEM,
 	},
 	.num_consumer_supplies  = ARRAY_SIZE(sdp4430_vdac_supply),
 	.consumer_supplies      = sdp4430_vdac_supply,
