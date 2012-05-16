AliceOps/android_kernel_samsung_msm8660-common

ASoC: twl6040: Configure init gain to minimal value
Set amplifier gain to minimal value after the driver is probed.
Today gain likes HSGAIN is set to maximum vaule. So at the first
playback the gain will no be configured. Default value should be
the minimal gain after power up.

Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

sound/soc/codecs/twl6040.c
@@ -120,15 +120,15 @@ static const u8 twl6040_reg[TWL6040_CACHEREGNUM] = {
 	0x18, /* TWL6040_MICLCTL	0x0C	- No input selected on Left Mic */
 	0x18, /* TWL6040_MICRCTL	0x0D	- No input selected on Right Mic */
 	0x00, /* TWL6040_MICGAIN	0x0E	*/
-	0x1B, /* TWL6040_LINEGAIN	0x0F	*/
+	0x00, /* TWL6040_LINEGAIN	0x0F	*/
 	0x00, /* TWL6040_HSLCTL		0x10	*/
 	0x00, /* TWL6040_HSRCTL		0x11	*/
-	0x00, /* TWL6040_HSGAIN		0x12	*/
-	0x00, /* TWL6040_EARCTL		0x13	*/
+	0xFF, /* TWL6040_HSGAIN		0x12	*/
+	0x1E, /* TWL6040_EARCTL		0x13	*/
 	0x00, /* TWL6040_HFLCTL		0x14	*/
-	0x00, /* TWL6040_HFLGAIN	0x15	*/
+	0x1D, /* TWL6040_HFLGAIN	0x15	*/
 	0x00, /* TWL6040_HFRCTL		0x16	*/
-	0x00, /* TWL6040_HFRGAIN	0x17	*/
+	0x1D, /* TWL6040_HFRGAIN	0x17	*/
 	0x00, /* TWL6040_VIBCTLL	0x18	*/
 	0x00, /* TWL6040_VIBDATL	0x19	*/
 	0x00, /* TWL6040_VIBCTLR	0x1A	*/

aosp/oz

sound/soc/codecs/twl6040.c
@@ -123,15 +123,15 @@ static const u8 twl6040_reg[TWL6040_CACHEREGNUM] = {
 	0x00, /* REG_MICLCTL	0x0C	*/
 	0x00, /* REG_MICRCTL	0x0D	*/
 	0x00, /* REG_MICGAIN	0x0E	*/
-	0x1B, /* REG_LINEGAIN	0x0F	*/
+	0x00, /* REG_LINEGAIN	0x0F	*/
 	0x00, /* REG_HSLCTL	0x10	*/
 	0x00, /* REG_HSRCTL	0x11	*/
-	0x00, /* REG_HSGAIN	0x12	*/
-	0x00, /* REG_EARCTL	0x13	*/
+	0xFF, /* REG_HSGAIN	0x12	*/
+	0x1E, /* REG_EARCTL	0x13	*/
 	0x00, /* REG_HFLCTL	0x14	*/
-	0x00, /* REG_HFLGAIN	0x15	*/
+	0x1D, /* REG_HFLGAIN	0x15	*/
 	0x00, /* REG_HFRCTL	0x16	*/
-	0x00, /* REG_HFRGAIN	0x17	*/
+	0x1D, /* REG_HFRGAIN	0x17	*/
 	0x00, /* REG_VIBCTLL	0x18	*/
 	0x00, /* REG_VIBDATL	0x19	*/
 	0x00, /* REG_VIBCTLR	0x1A	*/