369c632b018eaceb6342643d56ba369e1a512f4f
Subhajit Paul <subhajit_paul@ti.com>
2014-06-27 16:49:08 +0530

    Revert "Revert "drm: Perform ioctl command validation on the stored kernel values""
    This reverts commit 87c56dbced705c12ab982941be6f17c12ca9ab46.
    
    The required changes are done in SGX driver. So the actual revert
    is no longer necessary.
    
    Change-Id: Ib7070cbfa301a5b80a23b3958f609f7089b838c9
    Signed-off-by: Subhajit Paul <subhajit_paul@ti.com>

--- drivers/gpu/drm/drm_drv.c
+++ drivers/gpu/drm/drm_drv.c
@@ -399,6 +399,7 @@ long drm_ioctl(struct file *filp,
 		usize = asize = _IOC_SIZE(cmd);
 		if (drv_size > asize)
 			asize = drv_size;
+		cmd = ioctl->cmd_drv;
 	}
 	else if ((nr >= DRM_COMMAND_END) || (nr < DRM_COMMAND_BASE)) {
 		u32 drv_size;
